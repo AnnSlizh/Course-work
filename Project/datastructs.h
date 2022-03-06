8563bbabd4e810a481874351f59fdc2eab745877
annslizh <slizh20042004@gmail.com>
2022-03-06 20:56:04 +0300

    Add role, status, user

--- Project/datastructs.h
+++ Project/datastructs.h
@@ -1,2 +1,24 @@
 #pragma once
+#include <string>
+using namespace std;
+
+enum Role
+{
+	MAIN_ADMIN,
+	ADMIN,
+	USER
+};
+enum Status
+{
+	ACTIVE,
+	BLOCKED,
+	WAITING
+};
+struct user
+{
+	string login;
+	string password;
+	Role role;
+	Status status;
+};
 
